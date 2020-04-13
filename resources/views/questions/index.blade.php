91adb509075589f6114db4a0489c6a22a7d5046e
unknown <karthichamp99@gmail.com>
2020-04-13 20:56:09 +0530

    Added styles to counters

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
+@section('cdns')
+<style>
+    .counters {
+        margin-right: 30px;
+        font-size: 10px;
+        text-align: center;
+    }
+    strong {
+        display: block;
+        font-size: 2em;
+    }
+    .vote, .answer {
+        width: 60px;
+        height: 60px;
+    }
+</style>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -8,24 +26,35 @@
                 <div class="card-header">All Questions</div>
 
                 <div class="card-body">
-                    @foreach($questions as $question)
+                   @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . str_plural('view', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
-                            <h3 class="mt-0">
-                                <a href="{{ $question->url }}">{{ $question->title }}</a>
-                            </h3>
-                            <p class="lead">
-                                Asked By
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                <small class="text-muted">{{ $question->created_date }}</small>
-                            </p>
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
                                 {{ str_limit($question->body, 250) }}
                             </div>
                         </div>
                         <hr>
-                    @endforeach
+                   @endforeach
 
-                    {{ $questions->links() }}
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
                 </div>
             </div>
         </div>
