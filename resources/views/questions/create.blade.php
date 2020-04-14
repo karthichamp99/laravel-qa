ee98e45ff7cf1510848521070b34ee11b826b596
unknown <karthichamp99@gmail.com>
2020-04-14 20:33:29 +0530

    After creating question form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('cdns')
+<style>
+
+</style>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Questions</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('questions.store') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="question_title">Question Title</label>
+                            <input type="text" name="title" id="question_title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+
+                            @if ($errors->has('title'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="question_body">Question Body</label>
+                            <textarea name="body" id="question_body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask This Question</button>
+                        </div>
+                   </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
